091d62a9251b9f27b8bd0e5d2345db1e3d653014
thomscoder <moyelysian@icloud.com>
2021-12-13 21:31:21 +0100

    sharing initial functionalities

--- client/src/components/ScreenCapture.tsx
+++ client/src/components/ScreenCapture.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { startScreenRecording, stopScreenRecording } from '../scripts/ScreenCapture';
+import { shareScreenRecording, startScreenRecording, stopScreenRecording, stopScreenSharing } from '../scripts/ScreenCapture';
 import "../styles/_ScreenCapture.css";
 
 
 export default class ScreenCapture extends Component {
-    public videoRef: React.RefObject<HTMLVideoElement>;
+    private videoRef: React.RefObject<HTMLVideoElement>;
+    private videoSecondRef: React.RefObject<HTMLVideoElement>;
     private options: Object;
     constructor(props?: any) {
         super(props);
@@ -15,7 +16,10 @@ export default class ScreenCapture extends Component {
             audio: false,
         }
         this.videoRef = React.createRef();
+        this.videoSecondRef = React.createRef();
         this.startRecording = this.startRecording.bind(this);
+        this.shareRecording = this.shareRecording.bind(this);
+        this.stopSharing = this.stopSharing.bind(this);
         this.stopRecording = this.stopRecording.bind(this);
     }
 
@@ -23,15 +27,27 @@ export default class ScreenCapture extends Component {
         startScreenRecording(this.videoRef.current, this.options)
     }
 
+    shareRecording() {
+        shareScreenRecording(this.videoSecondRef.current, this.videoRef.current, this.options);
+    }
+    
+    stopSharing() {
+        stopScreenSharing(this.videoSecondRef.current)
+    }
+
     stopRecording() {
         stopScreenRecording(this.videoRef.current)
     }
+
     render() {
         return (
             <div>
                 <video id="video" ref={this.videoRef} autoPlay></video>
+                <video id="second-video" ref={this.videoSecondRef} autoPlay></video>
                 <div className="buttons">
                     <button type="button" onClick={this.startRecording}>Start recording</button>
+                    <button type="button" onClick={this.shareRecording}>Share recording</button>
+                    <button type="button" onClick={this.stopSharing}>Stop sharing</button>
                     <button type="button" onClick={this.stopRecording}>Stop recording</button>
                 </div>
             </div>
--- client/src/scripts/ScreenCapture.ts
+++ client/src/scripts/ScreenCapture.ts
@@ -1,7 +1,6 @@
 export const startScreenRecording = async (videoRef: HTMLVideoElement |null, options: any) => {
     try {
         videoRef!.srcObject = await navigator.mediaDevices.getDisplayMedia(options);
-        console.log(videoRef!.srcObject.getTracks())
         console.log(videoRef?.srcObject);
     }
     catch (err) {
@@ -17,4 +16,20 @@ export const stopScreenRecording = async (videoRef: HTMLVideoElement |null) => {
     catch (err) {
         console.log("Error", err);
     }
+    videoRef!.srcObject = null;
+}
+
+export const shareScreenRecording = async (videoRef: HTMLVideoElement |null, oldVideoRef: HTMLVideoElement | null, options: any) => {
+    try {
+        videoRef!.srcObject = oldVideoRef!.srcObject;
+        console.log(videoRef!.srcObject);
+    }
+    catch (err) {
+        console.log("Error", err);
+    }
+}
+
+export const stopScreenSharing = async (videoRef: HTMLVideoElement |null) => {
+    
+    videoRef!.srcObject = null;
 }
