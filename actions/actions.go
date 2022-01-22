4b8111b2f55139b7e1e6281798bc9844d9af9f40
thomscoder <moyelysian@icloud.com>
2022-01-22 22:45:30 +0100

    actions

---
+++ actions/actions.go
@@ -0,0 +1,22 @@
+package actions
+
+import (
+	"log"
+
+	"github.com/go-vgo/robotgo"
+)
+
+func MouseMove(x float64, y float64) {
+	log.Println(x, y)
+	robotgo.Move(int(x), int(y))
+}
+
+func MouseClick() {
+	robotgo.Click("left", true)
+	log.Println("mouse-click")
+}
+
+func KeyType(key string) {
+	robotgo.KeyTap(key)
+	log.Println("key-type")
+}
